10f42624f4f0f3c49ee5b5ea1f3e941602b391e6
clintandrewhall <clint@clintandrewhall.com>
2018-04-07 22:40:32 -0400

    Revising the About section

--- src/App.js
+++ src/App.js
@@ -1,67 +1,20 @@
 // @flow
 
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
+import About from './components/About.jsx';
 
-const BasicExample = () => (
+const App = () => (
   <Router>
     <div>
       <Header />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-        <Route component={NoMatch} />
-      </Switch>
+      <Home />
+      <About />
     </div>
   </Router>
 );
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
-
-const NoMatch = ({ location }) => (
-  <div>
-    <h3>
-      No match for <code>{location.pathname}</code>
-    </h3>
-  </div>
-);
-
-export default BasicExample;
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+// @flow
+
+import React from 'react';
+import classnames from 'classnames';
+
+import styles from './About.module.css';
+import gridStyles from './common/Grid.module.css';
+
+const About = () => {
+  return (
+    <section id="about" className={styles.about}>
+      <div className={styles.intro}>
+        <div className={styles.introContent}>
+          <h2 className={styles.title}>About Me</h2>
+          <h3 className={styles.subtitle}>I Love to Create Things</h3>
+          <p className={styles.lead}>
+            I'm a User Interface Engineer at Facebook, building new experiences
+            for desktop and mobile browsers. I love my work.
+          </p>
+          <p className={styles.lead}>
+            I enjoy conferences like{' '}
+            <a href="http://sxsw.com/" rel="external">
+              SxSW
+            </a>, gatherings like TED and TEDx, and hacks like{' '}
+            <a href="http://kansascity.startupweekend.org/" rel="external">
+              Startup Weekend
+            </a>. I love to build with Lego. I made my own desk. I tinker with
+            hardware and VR. You can occasionally find me onstage in local
+            community theatre.
+          </p>
+        </div>
+      </div>
+      <div className={styles.columns}>
+        <div className={styles.left}>
+          <h2 className={styles.columnTitle}>About My Work</h2>
+          <p>
+            I want to make the web “beautiful”, both on the surface and beneath
+            it. If I have a suggestion for a website or application, I prefer to
+            demonstrate the idea through an API rather than write a blog
+            proposing it.
+          </p>
+          <p>
+            I draw a distinction between “web sites” and “web applications”,
+            touting a strict progressive enhancement philosophy in the former,
+            (e.g. “the web is HTML”), and responsible use of front-end tech in
+            the latter, (focusing on performance, avoiding library bloat).
+          </p>
+          <p>
+            I’m comfortable with varying levels of skill in any web stack,
+            having written and maintained code in Ruby on Rails, PHP, and Java.
+            I currently prefer Node for most of my personal projects, as I find
+            I can get them up and running very quickly; I also love the very
+            active community.
+          </p>
+          <p>
+            I want to work in a “two-way street” environment, where I learn as
+            much as I contribute. I look for opportunities where I can make a
+            strong, direct and meaningful contribution to a team and to the web.
+            I strive to take the mysticism out of my profession, encouraging
+            anyone and everyone that creating things on the web was meant for
+            them, too.
+          </p>
+        </div>
+        <div className={styles.right}>
+          <h2 className={styles.columnTitle}>About My Code</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
