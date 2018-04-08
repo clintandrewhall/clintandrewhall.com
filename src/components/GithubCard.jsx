4eefd546f845a849d37369621ea94880511f7d06
clintandrewhall <clint@clintandrewhall.com>
2018-04-08 02:30:30 -0400

    A few tweaks before bed

--- src/components/GithubCard.jsx
+++ src/components/GithubCard.jsx
@@ -79,37 +79,63 @@ class GithubCard extends React.Component<Props, State> {
     } else {
       if (totalLoc) {
         let total = 0;
+        let other = 0;
 
         totalLoc.forEach((language: LOC) => {
           total += language.loc;
         });
 
-        let items = totalLoc.map((language: LOC, index: number) => {
-          const { name, loc } = language;
-          const color = languageColors[name] || 'inherit';
+        let items = totalLoc
+          .map((language: LOC, index: number) => {
+            const { name, loc } = language;
+            const percent = loc / total * 100;
 
-          return (
-            <li
-              className={styles.language}
-              key={'lang_' + index}
-              style={{ color }}>
+            if (percent < 1) {
+              other += loc;
+              return null;
+            }
+
+            const color = languageColors[name] || 'inherit';
+
+            return (
+              <li
+                className={styles.language}
+                key={'lang_' + index}
+                style={{ color }}>
+                <div
+                  className={styles.progress}
+                  style={{
+                    background: color,
+                    width: percent + '%',
+                  }}
+                />
+                <span className={styles.loc}>{numeral(loc).format('0a')}</span>
+                <span className={styles.languageName}>{name}</span>
+              </li>
+            );
+          })
+          .slice(0, other > 0 ? 9 : 10);
+
+        if (other > 0) {
+          const percent = other / total * 100;
+          items.push(
+            <li className={styles.language} key="other">
               <div
                 className={styles.progress}
                 style={{
-                  background: color,
-                  width: loc / total * 100 + '%',
+                  width: percent + '%',
                 }}
               />
-              <span className={styles.loc}>{numeral(loc).format('0a')}</span>
-              <span className={styles.languageName}>{name}</span>
-            </li>
+              <span className={styles.loc}>{numeral(other).format('0a')}</span>
+              <span className={styles.languageName}>Other</span>
+            </li>,
           );
-        });
+        }
 
         if (items.length > 0) {
           items = (
             <ul className={styles.languages} key="languages">
-              {items.slice(0, 10)}
+              {items}
             </ul>
           );
         }
