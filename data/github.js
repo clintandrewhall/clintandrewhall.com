0d6d1748de26a94a7a319a6681af7cb1b2befa43
clintandrewhall <clint@clintandrewhall.com>
2018-05-17 03:08:28 -0400

    Update Github fetching

--- data/github.js
+++ data/github.js
@@ -9,6 +9,9 @@ const writePath = './src/_content/github/';
 const userAPI = BASE_URL + username;
 const reposAPI = BASE_URL + username + '/repos';
 
+const values = <T>(obj: { [string]: T }): Array<T> =>
+  Object.keys(obj).map(k => obj[k]);
+
 (async function() {
   const responses = await Promise.all([fetch(userAPI), fetch(reposAPI)]);
   const jsons = await Promise.all(responses.map(result => result.json()));
@@ -20,9 +23,37 @@ const reposAPI = BASE_URL + username + '/repos';
     return;
   }
 
+  const repoReponses = await Promise.all(
+    repos.map((repo: Object) => fetch(repo.languages_url)),
+  );
+
+  const languages = await Promise.all(
+    repoReponses.map(result => result.json()),
+  );
+
+  let entries = {};
+
+  repos.forEach((repo: Object, index: number) => {
+    Object.entries(languages[index]).forEach(entry => {
+      const name = entry[0];
+      const loc = entry[1];
+      let item = entries[name] || { name, loc: 0 };
+      item.loc += parseInt(loc, 10) || 0;
+      entries[name] = item;
+    });
+  });
+
+  // FIXME: Using 'Object' is a bug; should be LOCType, but I'm getting a
+  // Flow error.
+  const loc = values(entries).sort(function(a: Object, b: Object) {
+    return b.loc - a.loc;
+  });
+
+  const result = { user, loc };
+
   fs.writeFile(
     writePath + 'index.js',
-    'export default ' + JSON.stringify(jsons),
+    'export default ' + JSON.stringify(result),
     () => {
       console.log('wrote github');
     },
--- src/components/Home/About/Github/index.js
+++ src/components/Home/About/Github/index.js
@@ -12,7 +12,8 @@ import backup from './../../../../_content/github';
 type Props = {};
 type State = {
   user: ?Object,
-  repos: ?Object,
+  live: boolean,
+  loaded: boolean,
   loc: ?Array<Object>,
 };
 
@@ -29,56 +30,61 @@ const values = <T>(obj: { [string]: T }): Array<T> =>
 
 class GithubCard extends React.Component<Props, State> {
   state = {
-    user: null,
-    repos: null,
-    loc: null,
+    user: backup.user,
+    loc: backup.loc,
+    live: false,
+    loaded: false,
   };
 
   async componentDidMount() {
     const responses = await Promise.all([fetch(userAPI), fetch(reposAPI)]);
-    let jsons = await Promise.all(responses.map(result => result.json()));
-    let user = jsons[0];
-    let repos = jsons[1];
+    const loaded = true;
 
-    if (repos.message || user.message) {
-      jsons = backup;
-      user = jsons[0];
-      repos = jsons[1];
+    if (responses[0].ok && responses[1].ok) {
+      const jsons = await Promise.all(
+        responses.map(response => response.json()),
+      );
+
+      const user = jsons[0];
+      const repos = jsons[1];
+
+      const repoReponses = await Promise.all(
+        repos.map((repo: Object) => fetch(repo.languages_url)),
+      );
+
+      const languages = await Promise.all(
+        repoReponses.map(result => result.json()),
+      );
+
+      let entries = {};
+
+      repos.forEach((repo: Object, index: number) => {
+        Object.entries(languages[index]).forEach(entry => {
+          const name = entry[0];
+          const loc = entry[1];
+          let item = entries[name] || { name, loc: 0 };
+          item.loc += parseInt(loc, 10) || 0;
+          entries[name] = item;
+        });
+      });
+
+      // FIXME: Using 'Object' is a bug; should be LOCType, but I'm getting a
+      // Flow error.
+      const loc = values(entries).sort(function(a: Object, b: Object) {
+        return b.loc - a.loc;
+      });
+
+      await this.setState({ user, loc, live: true, loaded });
+      return;
     }
 
-    const repoReponses = await Promise.all(
-      repos.map((repo: Object) => fetch(repo.languages_url)),
-    );
-
-    const languages = await Promise.all(
-      repoReponses.map(result => result.json()),
-    );
-
-    let entries = {};
-
-    repos.forEach((repo: Object, index: number) => {
-      Object.entries(languages[index]).forEach(entry => {
-        const name = entry[0];
-        const loc = entry[1];
-        let item = entries[name] || { name, loc: 0 };
-        item.loc += parseInt(loc, 10) || 0;
-        entries[name] = item;
-      });
-    });
-
-    // FIXME: Using 'Object' is a bug; should be LOCType, but I'm getting a
-    // Flow error.
-    const loc = values(entries).sort(function(a: Object, b: Object) {
-      return b.loc - a.loc;
-    });
-
-    await this.setState({ user, repos, loc });
+    await this.setState({ loaded });
   }
 
   render() {
-    const { user, loc, repos } = this.state;
+    const { user, loc, live, loaded } = this.state;
 
-    if (user === null || repos === null) {
+    if (!loaded) {
       return (
         <div className={styles.octocat}>
           <img src={octocat} alt="Loading" />
@@ -90,8 +96,9 @@ class GithubCard extends React.Component<Props, State> {
       <div className={styles.gh}>
         <div className={styles.card}>
           <p>
-            These lines-of-code counts and repository information are gathered
-            live, directly from my{' '}
+            These lines-of-code counts and repository information{' '}
+            {live ? 'are' : 'were'} gathered
+            {live ? 'live, ' : ''} directly from my{' '}
             <a
               href="https://www.github.com/clintandrewhall"
               rel="noopener noreferrer"
