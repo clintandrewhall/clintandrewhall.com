771ffe800be4f8bf7ffa05613062e746c23b536d
clintandrewhall <clint@clintandrewhall.com>
2018-08-24 16:58:17 -0500

    Refactoring to rewire, adding resume styles/sections.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import Pace from 'pace-js';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 // Pace.start();
 
 // Pace.on('done', function() {
 //   var event = new Event('pace-finished');
 //   window.dispatchEvent(event);
 // });
+import { unregister } from './registerServiceWorker';
+unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
