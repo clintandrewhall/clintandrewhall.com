fc9309c58ecf35e596fa872122920f4970e734cb
clintandrewhall <clint@clintandrewhall.com>
2018-08-24 18:47:39 -0500

    Fixing server js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 
 app.use('/resume', express.static(path.join(__dirname, 'build/resume')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
