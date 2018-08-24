b8b8da40a2db5f84f2859b290f3e1f7d4e5e8cd0
clintandrewhall <clint@clintandrewhall.com>
2018-08-24 14:44:15 -0500

    Rewiring

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const { compose, injectBabelPlugin } = require('react-app-rewired');
+const rewireCssModules = require('react-app-rewire-css-modules');
+
+module.exports = function(config, env){
+  const rewires = compose(
+    rewireCssModules,
+  );
+
+  return rewires(config, env);
+}
